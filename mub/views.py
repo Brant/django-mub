2585347f509fe38e7553830c03adc72f2c299039
Brant Steen <brant.steen@gmail.com>
2013-08-01 16:12:24 -0400

    This is not useful if its cache'd...

--- mub/views.py
+++ mub/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 
 from django.core.cache import cache
+from django.views.decorators.cache import never_cache
 
+@never_cache
 def cache_test(request):
     response_data = {
         "CSS_CACHE": cache.get("mub_css"),
