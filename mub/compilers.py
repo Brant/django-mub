4bde13613c7d7dba18bc8d0c0ebce278283e3cda
Brant Steen <brant.steen@gmail.com>
2013-10-09 10:29:45 -0400

    Just some dev settings

--- mub/compilers.py
+++ mub/compilers.py
@@ -36,37 +36,37 @@ class StaticCompiler(object):
     def _compile_file_list_from_staticfiles_dirs(self):
         """
         Compile list of static files for the given extension
-        
+
         Used when serving out of STATICFILES_DIRS (i.e. DEBUG = True)
         """
         finder = FileSystemFinder()
 
         for path, storage in finder.list(ignore_patterns=None):
             if path.endswith(".%s" % self._ext) and path.count(os.sep) < 2:
-                self._items.update({path.split(os.sep)[-1]: (path, storage.location)})        
+                self._items.update({path.split(os.sep)[-1]: (path, storage.location)})
 
     def _compile_file_list_from_static_root(self):
         """
         Compile list of statif files for the given extension
-        
+
         Used when serving out of STATIC_ROOT (i.e. DEBUG = False)
         """
         dir_list = []
         rootdir = settings.STATIC_ROOT
-        
+
         if rootdir.endswith("/") or rootdir.endswith("\\"):
             rootdir = rootdir[:-1]
-            
+
         for root, subFolders, files in os.walk(rootdir):
             if root.replace(rootdir, "").count(os.sep) == 1:
                 dir_list.append((root.replace("%s%s" % (rootdir, os.sep), ""), [a_file for a_file in files if a_file.endswith(".%s" % self._ext)]))
-        
+
         useable_list = []
-        
+
         for item in dir_list:
             if len(item[1]) > 0:
                 useable_list.append(item)
-        
+
         final_dict = {}
         for path, items in useable_list:
             for item in items:
@@ -76,7 +76,7 @@ class StaticCompiler(object):
     def clean_up(self):
         if self.cache_location and os.path.isdir(self.cache_location):
             shutil.rmtree(self.cache_location)
-    
+
     def get_staticfiles_list(self):
         """
         Return list of static files to serve
@@ -84,10 +84,10 @@ class StaticCompiler(object):
         if not settings.DEBUG:
             if self._file_from_cache:
                 return [self._file_from_cache]
-        
+
         self._massage_ordered_list()
         return [item[0] for item in self._ordered_items]
-    
+
     def _compile_file_list(self):
         """
         Return the list of files to feed to the template
@@ -99,19 +99,19 @@ class StaticCompiler(object):
             if cache.get(self._cache_key):
                 self._file_from_cache = cache.get(self._cache_key)
                 return
-            
+
             self._compile_file_list_from_static_root()
-        
+
         if self._items:
             self._location = self._items.values()[0][1]
-            
+
             cache_location = self._items.values()[0][0]
             cache_location = cache_location.split("/")
             cache_location[len(cache_location)-1] = "cache"
-      
+
             self._cache_key_path = "/".join(cache_location) + "/"
             self.cache_location = os.path.join(self._location, self._cache_key_path)
-      
+
             self.order_file_list()
 
     def order_file_list(self):
@@ -121,12 +121,12 @@ class StaticCompiler(object):
         params = getattr(settings, "MUB_%s_ORDER" % self._ext.upper(), ((), ()))
         top_files = params[0]
         bottom_files = params[1]
-        
+
         for filename in top_files:
             if filename in self._items:
                 self._ordered_items.append(self._items[filename])
                 del self._items[filename]
-        
+
         for_bottom = []
         for filename in bottom_files:
             if filename in self._items:
@@ -152,16 +152,16 @@ class StaticCompiler(object):
         """
         self._timestamp = latest_timestamp([os.path.join(location, path) for path, location in self._ordered_items])
         self._establish_filename()
-        
+
         cachefile = os.path.join(self.cache_location, self._filename)
 
         if not os.path.isfile(cachefile):
             minifier = MUBMinifier()
             minifier(self._ext, self._ordered_items, cachefile)
-        
+
         if lock:
             cache.set(self._cache_key, "%s%s" % (self._cache_key_path, self._filename), timeout=0)
-        
+
         self._ordered_items = [("%s%s" % (self._cache_key_path, self._filename), self.cache_location)]
 
     def _establish_filename(self):
