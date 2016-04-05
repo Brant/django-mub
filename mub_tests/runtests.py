9440d4b62b59a29cff43c82a08841d454dbfd5c6
Brant Steen <brant.steen@gmail.com>
2016-04-05 09:36:19 -0400

    updating test suite to django 1.9 compatibility

--- mub_tests/runtests.py
+++ mub_tests/runtests.py
@@ -3,13 +3,17 @@ import os
 import sys
 import imp
 
+import django
 from django.conf import settings
+from django.test.utils import get_runner
 
 PROJECT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 MY_INSTALLED_APPS = [
     'mub',
     'mub_tests',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
     'django.contrib.staticfiles',
 ]
 
@@ -20,15 +24,25 @@ try:
 except ImportError:
     pass
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, BASE_DIR)
+MY_TEST_RUNNER = 'django.test.runner.DiscoverRunner'
+
+try:
+    from django_nose.runner import NoseTestSuiteRunner
+    MY_TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
+except ImportError:
+    pass
+
 
 if not settings.configured:
     settings.configure(
+        BASE_DIR = BASE_DIR,
         DATABASES = {
             'default': {
                 'ENGINE': 'django.db.backends.sqlite3',
             }
         },
-        PROJECT_PATH = PROJECT_PATH,
         STATICFILES_DIRS = (PROJECT_PATH + "/static", ),
         STATICFILES_FINDERS = (
             'django.contrib.staticfiles.finders.FileSystemFinder',
@@ -39,33 +53,27 @@ if not settings.configured:
         STATIC_ROOT = PROJECT_PATH + "/collectstatic",
         ROOT_URLCONF = 'mub_tests.urls',
         NOSE_ARGS = [
-            "--with-xcoverage", 
-            "--cover-inclusive", 
-            "--with-xunit", 
+            "--with-xcoverage",
+            "--cover-inclusive",
+            "--with-xunit",
             "--exe",
-            "--verbosity=3", 
+            "--verbosity=3",
             "--cover-package=mub"
         ],
         NOSE_PLUGINS = [
             'nosexcover.XCoverage',
             "nose_exclude.NoseExclude"
         ],
+        TEST_RUNNER = MY_TEST_RUNNER,
     )
 
 
-try:
-    from django_nose.runner import NoseTestSuiteRunner
-except ImportError:
-    from django.test.simple import DjangoTestSuiteRunner
-
-
 def runtests():
-    try:
-        runner = NoseTestSuiteRunner()
-    except NameError:
-        runner = DjangoTestSuiteRunner()
-    failures = runner.run_tests(['mub_tests'])
-    sys.exit(failures)
+    django.setup()
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner()
+    failures = test_runner.run_tests(["mub_tests"])
+    sys.exit(bool(failures))
 
 
 if __name__ == '__main__':
