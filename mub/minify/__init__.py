ef8df9ca3c78a549981b36d6aea5536cae018715
Brant Steen <brant.steen@gmail.com>
2016-04-05 10:36:57 -0400

    playing around

--- mub/minify/__init__.py
+++ mub/minify/__init__.py
@@ -14,7 +14,7 @@ from .js_min import jsmin
 from mub.util import massage_css_images_for_cache_path
 
 class MUBMinifier:
-    
+
     def __call__(self, ext, filelist, cachefile):
         self._filelist = filelist
         self._cachefile = cachefile
@@ -24,7 +24,7 @@ class MUBMinifier:
         mini_method = getattr(self, "mini_%s" % self._ext)
         mini_method()
         self.save()
-        
+
     def build_string(self):
         for filename, location in self._filelist:
             the_file = open(os.path.join(location, filename))
@@ -34,15 +34,15 @@ class MUBMinifier:
     def mini_css(self):
         """
         """
-        css_url = settings.STATIC_URL + os.sep.join(self._filelist[0][0].split(os.sep)[:-1]) 
+        css_url = settings.STATIC_URL + os.sep.join(self._filelist[0][0].split(os.sep)[:-1])
         self._str = cssmin(massage_css_images_for_cache_path(smart_unicode(self._str), css_url))
-        
+
     def mini_js(self):
         """
-        """        
+        """
         self._str = jsmin(render_to_string("mub/js_config.txt", {"STATIC_URL": settings.STATIC_URL}) + smart_unicode(self._str))
 
-        
+
     def save(self):
         save_dir = os.path.dirname(os.path.abspath(self._cachefile))
         if not os.path.isdir(save_dir):
@@ -50,4 +50,4 @@ class MUBMinifier:
         the_file = open(self._cachefile, "w")
         the_file.write(self._str)
         the_file.close()
-    
+
