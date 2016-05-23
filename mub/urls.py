05f4150145133393c8ba250b42e3cb9083cf58cb
Brant Steen <brant.steen@gmail.com>
2016-05-23 15:13:00 -0400

    urls updates for latest django(s)

--- mub/urls.py
+++ mub/urls.py
@@ -1,9 +1,11 @@
 """
 URL configurations
 """
-from django.conf.urls import patterns, url
+from django.conf.urls import url
+
+from mub.views import cache_test
 
 
-urlpatterns = patterns('mub.views',
-    url(r'^cachetest/$', 'cache_test', name='mub_cache_test'),
-)
+urlpatterns = [
+    url(r'^cachetest/$', cache_test, name='mub_cache_test'),
+]
